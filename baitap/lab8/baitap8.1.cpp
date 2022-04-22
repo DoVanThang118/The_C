e7a28c0040471fd5a6713716ba41b372d6094b67
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2022-04-22 15:58:44 +0700

    baitap8

---
+++ baitap/lab8/baitap8.1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "baitap8.h"
+int main(){
+	int a;
+	printf ("nhap: ");
+	scanf ("%d",&a);
+	int b;
+	printf ("nhap: ");
+	scanf ("%d",&b);
+	printf (" la: %f",tinhLuyThua(a,b));
+}
