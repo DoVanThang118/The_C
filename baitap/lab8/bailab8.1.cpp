683264294c5bfb13428f78d62bcd054f6c932b50
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2022-04-25 14:53:28 +0700

    bailab8

---
+++ baitap/lab8/bailab8.1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "bailab8.h"
+int main(){
+	int a;
+	printf ("nhap  :");
+	scanf ("%d",&a);
+	int b;
+	printf ("nhap  :");
+	scanf ("%d",&b);
+	
+	printf (" la: %d",BCNN(a,b));
+}
